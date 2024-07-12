3b7369176e581a6aaeb66334138c6b609ddc72a9
Gabe Cook <gabe565@gmail.com>
2024-07-11 23:57:54 -0500

    feat: Initial commit

---
+++ game.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Game struct {
+	tiles [][]int
+}
+
+func (g Game) Init() tea.Cmd {
+	return Tick
+}
+
+func (g Game) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tick:
+		for y, row := range g.tiles {
+			for x, cell := range row {
+				var neighbors int
+				if y > 0 {
+					if x > 0 {
+						neighbors += g.tiles[y-1][x-1]
+					}
+					neighbors += g.tiles[y-1][x]
+					if x < len(row)-1 {
+						neighbors += g.tiles[y-1][x+1]
+					}
+				}
+				if x > 0 {
+					neighbors += row[x-1]
+				}
+				if x < len(row)-1 {
+					neighbors += row[x+1]
+				}
+				if y < len(g.tiles)-1 {
+					if x > 0 {
+						neighbors += g.tiles[y+1][x-1]
+					}
+					neighbors += g.tiles[y+1][x]
+					if x < len(row)-1 {
+						neighbors += g.tiles[y+1][x+1]
+					}
+				}
+
+				switch {
+				case neighbors <= 1, neighbors >= 4:
+					if cell == 1 {
+						defer func() {
+							row[x] = 0
+						}()
+					}
+				case neighbors == 3:
+					if cell == 0 {
+						defer func() {
+							row[x] = 1
+						}()
+					}
+				}
+			}
+		}
+		return g, Tick
+	case tea.WindowSizeMsg:
+		g.tiles = make([][]int, msg.Height)
+		for i := range msg.Height {
+			g.tiles[i] = make([]int, msg.Width)
+		}
+	case tea.MouseMsg:
+		switch msg.Action {
+		case tea.MouseActionPress, tea.MouseActionMotion:
+			if len(g.tiles) > msg.Y && len(g.tiles[msg.Y]) > msg.X {
+				g.tiles[msg.Y][msg.X] = 1
+			}
+		}
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "r":
+			for _, row := range g.tiles {
+				for i := range row {
+					row[i] = 0
+				}
+			}
+		case "ctrl+c", "q", "esc":
+			return g, tea.Quit
+		}
+	}
+	return g, nil
+}
+
+func (g Game) View() string {
+	var view string
+	for _, row := range g.tiles {
+		for _, cell := range row {
+			if cell == 1 {
+				view += "█"
+			} else {
+				view += " "
+			}
+		}
+		view += "\n"
+	}
+	return view
+}
+
+type tick struct{}
+
+func Tick() tea.Msg {
+	time.Sleep(time.Second / 15)
+	return tick{}
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	program := tea.NewProgram(
+		Game{},
+		tea.WithAltScreen(),
+		tea.WithMouseCellMotion(),
+	)
+	if _, err := program.Run(); err != nil {
+		slog.Error("Error running game", "error", err.Error())
+		os.Exit(1)
+	}
+}
