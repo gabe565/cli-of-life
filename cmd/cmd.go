1043a4aa42c47d33cd11e0395e2a634cb646e025
Gabe Cook <gabe565@gmail.com>
2024-07-12 14:20:16 -0500

    feat(cmd): Add shell completions

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/gabe565/cli-of-life/internal/config"
 	"github.com/gabe565/cli-of-life/internal/game"
 	"github.com/gabe565/cli-of-life/internal/pattern"
 	"github.com/spf13/cobra"
@@ -11,14 +12,32 @@ func New() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "cli-of-life",
 		RunE: run,
+		Args: cobra.NoArgs,
+
+		ValidArgsFunction: cobra.NoFileCompletions,
 	}
-	cmd.Flags().StringP("file", "f", "", "Loads a pattern file on startup")
+
+	cmd.Flags().StringP(config.FileFlag, "f", "", "Loads a pattern file on startup")
+	if err := cmd.RegisterFlagCompletionFunc(config.FileFlag,
+		func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return []string{".rle", ".cells"}, cobra.ShellCompDirectiveFilterFileExt
+		},
+	); err != nil {
+		panic(err)
+	}
+
+	cmd.Flags().String(config.CompletionFlag, "", "Output command-line completion code for the specified shell. Can be 'bash', 'zsh', 'fish', or 'powershell'.")
+
 	return cmd
 }
 
 func run(cmd *cobra.Command, args []string) error {
+	if shell := cmd.Flag(config.CompletionFlag).Value.String(); shell != "" {
+		return completion(cmd, shell)
+	}
+
 	var tiles [][]int
-	if file := cmd.Flag("file").Value.String(); file != "" {
+	if file := cmd.Flag(config.FileFlag).Value.String(); file != "" {
 		var err error
 		if tiles, err = pattern.UnmarshalFile(file); err != nil {
 			return err
