cb06e4546b5d712cb9efe3d158a673b8ad08a39b
Gabe Cook <gabe565@gmail.com>
2024-11-14 13:46:29 -0600

    fix(menu): Add `w`/`s` keybinds to forms

--- internal/game/util/huh.go
+++ internal/game/util/huh.go
@@ -8,11 +8,18 @@ import (
 func NewForm(groups ...*huh.Group) *huh.Form {
 	keymap := huh.NewDefaultKeyMap()
 	keymap.Quit = addKeys(keymap.Quit, "esc")
+
+	keymap.Select.Up = addKeys(keymap.Select.Up, "w")
+	keymap.Select.Down = addKeys(keymap.Select.Down, "s")
 	keymap.Select.Submit = addKeys(keymap.Select.Submit, " ")
+
 	keymap.FilePicker.Open.SetEnabled(false)
 	keymap.FilePicker.Open = addKeys(keymap.FilePicker.Open, " ")
 	keymap.FilePicker.Select = addKeys(keymap.FilePicker.Select, " ")
 	keymap.FilePicker.Submit = addKeys(keymap.FilePicker.Submit, " ")
+	keymap.FilePicker.Up = addKeys(keymap.FilePicker.Up, "w")
+	keymap.FilePicker.Down = addKeys(keymap.FilePicker.Down, "s")
+
 	keymap.Text.NewLine.SetKeys()
 
 	return huh.NewForm(groups...).WithKeyMap(keymap)
